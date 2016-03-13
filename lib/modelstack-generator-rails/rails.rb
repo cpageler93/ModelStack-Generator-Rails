d153781872ac3bc55f0aff1a09481cecd8075fa5
Christoph Pageler <christoph.pageler@ewetel.net>
2016-03-13 20:33:22 +0100

    added base methods for generation

--- lib/modelstack-generator-rails/rails.rb
+++ lib/modelstack-generator-rails/rails.rb
@@ -5,13 +5,44 @@ module ModelStack
     class Rails
       include ModelStack::Generator::Base
 
+
+      attr_accessor :rails_dir
+
+
+      ############################
+      ##      BASE METHODS      ##
+      ############################
+
       def handle_options(options)
-        puts "handle options #{options}"
+        # puts "handle options #{options}"
       end
 
       def generate(data_model)
-        puts "output to path #{self.absolute_output_path}"
-        puts "generate data model"
+
+        # get rails name
+        rails_app_name = get_rails_app_name(data_model[:name])
+
+        # get scopes
+        scopes = data_model[:scopes]
+
+        # set rails dir
+        self.rails_dir = File.join(self.absolute_output_path, rails_app_name)
+
+        ##########################
+        # START GENERATION
+
+      end
+
+      ############################
+      ##     CUSTOM METHODS     ##
+      ############################
+
+      def get_rails_app_name(raw_name)
+        rails_app_name = raw_name
+        rails_app_name.downcase!
+        rails_app_name.gsub!(' ', '_')
+
+        return rails_app_name
       end
 
     end
