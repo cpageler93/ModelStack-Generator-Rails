72d1e41f1a2f546fd43545e07be6ddf0b33e3d27
Christoph Pageler <christoph.pageler@ewetel.net>
2016-03-13 15:57:35 +0100

    updated to new modelstack generator style

--- lib/modelstack-generator-rails/rails.rb
+++ lib/modelstack-generator-rails/rails.rb
@@ -1,9 +1,17 @@
+require 'modelstack'
+
 module ModelStack
   module Generator
     class Rails
+      include ModelStack::Generator::Base
 
-      def self.generate(options)
-        puts "generate rails with options #{options}"
+      def handle_options(options)
+        puts "handle options #{options}"
+      end
+
+      def generate(data_model)
+        puts "output to path #{self.absolute_output_path}"
+        puts "generate data model"
       end
 
     end
