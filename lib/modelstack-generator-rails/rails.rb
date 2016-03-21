93181401b562b60362d3afebdda2c80efe348e8f
Christoph Pageler <christoph.pageler@ewetel.net>
2016-03-21 22:15:37 +0100

    added counting and dummy enumeration for scopes, controllers and actions

--- lib/modelstack-generator-rails/rails.rb
+++ lib/modelstack-generator-rails/rails.rb
@@ -19,13 +19,48 @@ module ModelStack
       end
 
       def number_of_steps
-        return 3
+        i = 0
+
+        i += 1 # step 1
+        i += 1 # step 2
+        i += 1 # step 3
+
+        # step 4
+        data_model[:scopes].each do |scope|
+          i += number_of_steps_for_scope(scope)
+        end
+
+        return i
+      end
+
+      def number_of_steps_for_scope(scope)
+        i = 1
+        scope.controllers.each do |controller|
+          i += number_of_steps_for_controller(controller)
+        end
+        return i
+      end
+
+      def number_of_steps_for_controller(controller)
+        i = 1
+
+        controller.actions.each do |action|
+          i += 1
+        end
+
+        controller.child_controllers.each do |child_controller|
+          i += number_of_steps_for_controller(child_controller)
+        end
+
+        return i
       end
 
       def generate
 
+        # get data model
         app_name = data_model[:name]
-        scopes = data_model[:scopes]
+        models   = data_model[:models]
+        scopes   = data_model[:scopes]
 
         # create some variables
         self.rails_app_name = get_rails_app_name(app_name)
@@ -33,7 +68,8 @@ module ModelStack
         self.rails_dir = File.join(self.absolute_output_path, rails_app_name)
 
         ##########################
-        # START GENERATION
+        #    START GENERATION    #
+        ##########################
 
         # Step 1: create output folder
         Proc.new {
@@ -44,7 +80,7 @@ module ModelStack
           create_output_folder
         }.call
 
-        # Step 2: Copy Template to destination
+        # Step 2: Copy template to destination
         template_dir = File.join(self.absolute_gem_path, "template")
         Proc.new {
           inc_step
@@ -70,8 +106,53 @@ module ModelStack
           fill_out_file(path_for_file("config/initializers/session_store.rb"), {
             'rails_app_session_store_key' => session_key
           })
-
         }.call
+
+        # Step 4: enumerate scopes (prepare, no call)
+        step_4_1 = nil
+        step_4_2 = nil
+        step_4 = Proc.new {
+          update_title "enumerate scopes"
+          scopes.each do |scope|
+            inc_step
+
+            if scope.controllers.length > 0
+              log_message "#{scope.path} - controllers: #{scope.controllers.length}"
+              scope.controllers.each do |controller|
+                step_4_1.call(nil, controller)
+              end
+            else
+              log_message "#{scope.path} - nothing to do"
+            end
+
+          end
+        }
+
+        # Step 4.1: enumerate controllers (prepare)
+        step_4_1 = Proc.new { |parent_controllers, controller|
+          inc_step
+          p = parent_controllers ? parent_controllers.collect{|c|c.identifier} : "<null>"
+          log_message "handle controller #{controller.identifier} in parent #{p}"
+
+          controller.actions.each do |action|
+            step_4_2.call(action)
+          end
+
+          controller.child_controllers.each do |child_controller|
+            parent_controllers ||= []
+            parent_controllers << controller
+            step_4_1.call(parent_controllers, child_controller)
+          end
+        }
+
+        # Step 4.2: enumerate actions (prepare)
+        step_4_2 = Proc.new { |action|
+          inc_step
+          log_message "handle action #{action.identifier}"
+        }
+
+        # Step 4: call
+        step_4.call
 
       end
 
